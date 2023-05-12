ac0acba80cd57659e9556551f5bdb72fde673e8a
laura <laura@LAPTOP-9BP06H1P>
2023-05-12 14:11:17 -0500

    Se realizo el ejemplo de patron Template Method

--- designPatterns/src/co/edu/poli/templatemethod/model/PizzaHawaiana.java
+++ designPatterns/src/co/edu/poli/templatemethod/model/PizzaHawaiana.java
@@ -2,6 +2,7 @@ package co.edu.poli.templatemethod.model;
 
 public class PizzaHawaiana extends Pizza {
     // Implementación específica de agregarIngredientes para PizzaHawaiana
+	@Override
     protected void agregarIngredientes() {
         System.out.println("Agregando jamón, piña y queso...");
     }
--- designPatterns/src/co/edu/poli/templatemethod/model/PizzaPepperoni.java
+++ designPatterns/src/co/edu/poli/templatemethod/model/PizzaPepperoni.java
@@ -2,6 +2,7 @@ package co.edu.poli.templatemethod.model;
 
 public class PizzaPepperoni extends Pizza {
     // Implementación específica de agregarIngredientes para PizzaPepperoni
+	@Override
     protected void agregarIngredientes() {
         System.out.println("Agregando pepperoni y queso...");
     }
